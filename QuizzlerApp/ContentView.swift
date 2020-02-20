3b9f43e82b77d0b5accd2e585da571c7dab13138
naciribraimo <nacibraimo@gmail.com>
2020-02-20 08:04:19 +0200

    Initial Commit

---
+++ QuizzlerApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuizzlerApp
+//
+//  Created by Nacir Ibraimo on 2/20/20.
+//  Copyright © 2020 Nacir Ibraimo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
